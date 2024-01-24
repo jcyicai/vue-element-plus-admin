0b36bfb74046cc3abf5e7d458f73a2a56997ba76
Jason Chen <chenyicai622@qq.com>
2024-01-24 16:47:14 +0800

    feat(优化): 配置调整

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-config-provider :locale="locale" :size="size">
+  <el-config-provider :locale="locale" :size="elementSize">
     <router-view></router-view>
   </el-config-provider>
 </template>
@@ -11,14 +11,18 @@ import { getBrowserLang } from '@/utils'
 import en from 'element-plus/es/locale/lang/en'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
+// 全局配置
 const globalStore = useGlobalStore()
 
-// element language
+// 语言 默认根据系统
 const locale = computed(() => {
-  if (globalStore.language == 'zh') return zhCn
-  if (globalStore.language == 'en') return en
-  return getBrowserLang() == 'zh' ? zhCn : en
+  if (globalStore.language === 'zh') return zhCn
+  if (globalStore.language === 'en') return en
+  return getBrowserLang() === 'zh' ? zhCn : en
 })
+
+// element 组件大小
+const elementSize = computed(() => globalStore.elementSize)
 </script>
 
 <style scoped></style>
